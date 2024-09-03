dced230be94beb35ac3077b77a314a8ed04afd74
max <me@mclifford.dev>
2024-09-03 14:42:04 +0100

    Added CardComponent.tsx

---
+++ src/components/web-builder-components/CardComponent.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Card} from "@/components/ui/card.tsx";
+import {ContainerComponent} from "@/components/web-builder-components/ContainerComponent.tsx";
+import {TextComponent} from "@/components/web-builder-components/TextComponent.tsx";
+import {ButtonComponent} from "@/components/web-builder-components/ButtonComponent.tsx";
+
+interface CardComponentProps {
+    background: string
+    padding: string
+}
+
+export const CardComponent = ({background, padding}: CardComponentProps) => {
+    return (
+        <Card>
+            <ContainerComponent background={background} padding={padding} margin={'0'}>
+                <div className='text-only'>
+                    <TextComponent text="Title" fontSize="20px" />
+                    <TextComponent text="Description" fontSize="16px" />
+                </div>
+                <div className='buttons-only'>
+                    <ButtonComponent size="sm" variant="outline" color="primary">
+                        Learn more
+                    </ButtonComponent>
+                </div>
+
+            </ContainerComponent>
+        </Card>)
+}

8177c52e3e61667b12db80def414609a96c39860
max <me@mclifford.dev>
2024-09-04 21:38:14 +0100

    Added page-view.tsx

---
+++ src/components/editor/page-view.tsx
@@ -0,0 +1,15 @@
+import {Element} from "@craftjs/core";
+import {ContainerComponent} from "@/components/web-builder-components/ContainerComponent.tsx";
+import {TextComponent} from "@/components/web-builder-components/TextComponent.tsx";
+import {ButtonComponent} from "@/components/web-builder-components/ButtonComponent.tsx";
+
+export const PageView = () => {
+    return (<div className="w-[calc(100%_-_3.5rem-20%)] h-full bg-gray-200 flex flex-col items-center ">
+        <div className="mt-4 w-4/5">
+            <Element id="root_container" is={ContainerComponent} canvas background={"white"} padding={""} margin={""}>
+                <TextComponent text={"This is some text"} fontSize={"1rem"}></TextComponent>
+                <ButtonComponent size="sm" variant="outline" color="primary">Press me!</ButtonComponent>
+            </Element>
+        </div>
+    </div>)
+}

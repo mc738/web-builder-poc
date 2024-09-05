03852b3bce78fa33d008c2e825ff5aa6ca21db91
max <me@mclifford.dev>
2024-09-05 21:19:31 +0100

    Work on page-view.tsx

--- src/components/editor/page-view.tsx
+++ src/components/editor/page-view.tsx
@@ -2,6 +2,7 @@ import {Element} from "@craftjs/core";
 import {ContainerComponent} from "@/components/web-builder-components/ContainerComponent.tsx";
 import {TextComponent} from "@/components/web-builder-components/TextComponent.tsx";
 import {ButtonComponent} from "@/components/web-builder-components/ButtonComponent.tsx";
+import {CardComponent} from "@/components/web-builder-components/CardComponent.tsx";
 
 export const PageView = () => {
     return (<div className="w-[calc(100%_-_3.5rem-20%)] h-full bg-gray-200 flex flex-col items-center ">
@@ -9,6 +10,7 @@ export const PageView = () => {
             <Element id="root_container" is={ContainerComponent} canvas background={"white"} padding={""} margin={""}>
                 <TextComponent text={"This is some text"} fontSize={"1rem"}></TextComponent>
                 <ButtonComponent size="sm" variant="outline" color="primary">Press me!</ButtonComponent>
+                <CardComponent background={"white"} padding={""}></CardComponent>
             </Element>
         </div>
     </div>)

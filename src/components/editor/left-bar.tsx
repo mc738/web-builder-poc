0e79baaa0b5689ba361746a4cced02497c9c0334
max <me@mclifford.dev>
2024-09-06 15:53:56 +0100

    Work on left-bar.tsx

--- src/components/editor/left-bar.tsx
+++ src/components/editor/left-bar.tsx
@@ -1,11 +1,17 @@
 import {Button} from "@/components/ui/button.tsx";
 import {Type} from "lucide-react";
+import {useEditor} from "@craftjs/core";
+import {TextComponent} from "@/components/web-builder-components/TextComponent.tsx";
 
 export const LeftBar = () => {
-    return(
+    const {connectors/*, query*/} = useEditor();
+
+    return (
         <aside className="hidden h-full w-14 flex-col border-r bg-background sm:flex items-center pt-4">
-            <Button variant="ghost" size="icon">
-                <Type></Type>
+            <Button ref={ref => connectors.create(ref!, <TextComponent text={"Hello, World!"} fontSize={"1rem"}/>)}
+                    variant="ghost"
+                    size="icon">
+                <Type/>
             </Button>
         </aside>
     )

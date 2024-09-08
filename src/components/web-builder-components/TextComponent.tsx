034b3ced458584d7b69b0d0662f6bcad6e704f60
max <me@mclifford.dev>
2024-09-08 13:35:43 +0100

    Work on editable text

--- src/components/web-builder-components/TextComponent.tsx
+++ src/components/web-builder-components/TextComponent.tsx
@@ -1,5 +1,6 @@
 import {Node, useNode} from '@craftjs/core';
 import ContentEditable from "react-contenteditable";
+import {useEffect, useState} from "react";
 
 
 interface TextComponentProps {
@@ -8,10 +9,21 @@ interface TextComponentProps {
 }
 
 export const TextComponent = ({text, fontSize}: TextComponentProps) => {
-    const {connectors: {connect, drag}, actions: {setProp}} = useNode();
+    const {connectors: {connect, drag}, hasSelectedNode, /*hasDraggedNode,*/ actions: {setProp}} = useNode((state) => ({
+        hasSelectedNode: state.events.selected,
+        hasDraggedNode: state.events.dragged,
+    }));
+
+    const [editable, setEditable] = useState(false);
+
+    useEffect(() => {
+        !hasSelectedNode && setEditable(false);
+    }, [hasSelectedNode]);
 
     return (
-        <div ref={ref => connect(drag(ref!))}>
+        <div
+            ref={ref => connect(drag(ref!))}
+            onClick={_ => setEditable(true)}>
             <ContentEditable
                 html={text}
                 onChange={e =>
@@ -19,6 +31,7 @@ export const TextComponent = ({text, fontSize}: TextComponentProps) => {
                         props.text = e.target.value.replace(/<\/?[^>]+(>|$)/g, ""))}
                 tagName="p"
                 style={{fontSize}}
+                disabled={!editable}
             ></ContentEditable>
         </div>
     );

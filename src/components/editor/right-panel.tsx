bd3fec548d1e3879a93d9bf2a997658f922652d3
max <me@mclifford.dev>
2024-09-08 18:37:55 +0100

    Work on right panel

--- src/components/editor/right-panel.tsx
+++ src/components/editor/right-panel.tsx
@@ -1,7 +1,9 @@
+import {SettingsPanel} from "@/components/editor/settings-panel.tsx";
+
 export const RightPanel = () => {
     return (
         <div className="w-1/5 h-full border-l">
-
+            <SettingsPanel></SettingsPanel>
         </div>
     )
 }

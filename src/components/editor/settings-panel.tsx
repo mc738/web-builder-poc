4babe6af071d7b4ae03d0e9e33dd2885269527e8
max <me@mclifford.dev>
2024-09-08 18:38:07 +0100

    Work on settings-panel.tsx

--- src/components/editor/settings-panel.tsx
+++ src/components/editor/settings-panel.tsx
@@ -1,14 +1,33 @@
 import {Badge} from "@/components/ui/badge.tsx";
+import {useEditor} from "@craftjs/core";
+import React from "react";
 
 export const SettingsPanel = () => {
-    return(
+
+    const {selected} = useEditor((state) => {
+        const [currentNodeId] = state.events.selected;
+        let selected;
+
+        if (currentNodeId) {
+            selected = {
+                id: currentNodeId,
+                name: state.nodes[currentNodeId].data.name,
+                settings: state.nodes[currentNodeId].related && state.nodes[currentNodeId].related.settings,
+            };
+        }
+
+        return {selected}
+    });
+
+    return selected ? (
         <div>
             <div className="grid grid-cols-1">
                 <div className="items-center">
                     <h1>Selected</h1>
                     <Badge></Badge>
+                    {selected.settings && React.createElement(selected.settings)}
                 </div>
             </div>
         </div>
-    );
+    ) : null
 };

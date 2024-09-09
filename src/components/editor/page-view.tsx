9ea968e9c6d3411169501b7cac1436ddbbd4d124
max <me@mclifford.dev>
2024-09-09 16:39:28 +0100

    Work on page-view.tsx

--- src/components/editor/page-view.tsx
+++ src/components/editor/page-view.tsx
@@ -8,7 +8,7 @@ export const PageView = () => {
     return (<div className="w-[calc(100%_-_3.5rem-20%)] h-full bg-gray-200 flex flex-col items-center ">
         <div className="mt-4 w-4/5">
             <Element id="root_container" is={ContainerComponent} canvas background={"white"} padding={""} margin={""}>
-                <TextComponent text={"This is some text"} fontSize={"1rem"}></TextComponent>
+                <TextComponent text={"This is some text"} fontSize={"1"} fontSizeUnits={"rem"}></TextComponent>
                 <ButtonComponent size="sm" variant="outline" color="primary">Press me!</ButtonComponent>
                 <CardComponent background={"white"} padding={""}></CardComponent>
             </Element>
